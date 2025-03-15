514acdce6c0fe9e876d5bde139e4b9a87bd32d7c
Luis Quintino <luis.virtual@outlook.com>
2025-03-14 18:16:33 -0600

    cs50 2025 problem 1.2, new probing approach

--- pset1/mario-more/mario.c
+++ pset1/mario-more/mario.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
+void construct_pyramid();
 int main(void)
 {
     int tallness;
 
     printf("Say, how big do you want the pyramid to be? ");
     scanf("%d", &tallness);
-    printf("I'll do my best to print this %d-story pyramid\n", tallness);
+    construct_pyramid(tallness);
 }
 
+void construct_pyramid(int height)
+{
+    /* while(height > 0)
+    {
+        printf("#  #\n");
+        height--;
+    }*/
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
