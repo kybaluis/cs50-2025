68c00bbe61b52f732741b6b89b14ba94ea232a8a
Luis Quintino <luis.virtual@outlook.com>
2025-03-16 17:56:19 -0600

    CS50 2025 problem 2.1, functionality correct, lots of tweaks to be made though

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int scrabble_scoring(char word[]);
 
 int main(void)
 {
-    char word1[50];
-    char word2[50];
+    char word1[50], word2[50];
 
     printf("Enter Player #1's word: ");
     scanf("%49s", word1);
     printf("Enter Player #2's word: ");
     scanf("%49s", word2);
+
     int score1 = scrabble_scoring(word1);
     int score2 = scrabble_scoring(word2);
 
@@ -24,4 +28,47 @@ int main(void)
     {
         printf("Tie!\n");
     }
+    printf("Player 1 score is: %d", score1);
+    printf("Player 2 score is: %d", score2);
+}
+
+int scrabble_scoring(char word[])
+{
+    int score = 0;
+    int word_length = strlen(word);
+    for (int i = 0, length = strlen(word); i < length; i++)
+    {
+        word[i] = tolower(word[i]);
+        if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i'
+            || word[i] == 'l' || word[i] == 'n' || word[i] == 'o' 
+            || word[i] == 'r' || word[i] == 's' || word[i] == 't' || word[i] == 'u')
+        {
+            score += 1;
+        }
+        else if (word[i] == 'd' || word[i] == 'g')
+        {
+            score += 2;
+        }
+         else if (word[i] == 'b' || word[i] == 'c' || word[i] == 'm' || word[i] == 'p')
+        {
+            score += 3;
+        }
+        else if (word[i] == 'f' || word[i] == 'h'|| word[i] == 'v'|| word[i] == 'w' || word[i] == 'y')
+        {
+            score += 4;
+        }
+        else if (word[i] == 'k')
+        {
+            score += 5;
+        }
+        else if (word[i] == 'j' || word[i] == 'x')
+        {
+            score += 8;
+        }
+        else if (word[i] == 'q' || word[i] == 'z')
+        {
+            score += 10;
+        }
+    }
+    return score;
 }
