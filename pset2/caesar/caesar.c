b9a4c40c53f59e9a5e795decbaf23ca552da0d2e
Luis Quintino <luis.virtual@outlook.com>
2025-03-18 20:46:10 -0600

    cs50 2025 problem 2.3 comments added; minor improvements too

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -7,28 +7,33 @@ void caesar_cipher(int k, char plain[], char cipher[]);
 
 int main(int argc, char *argv[])
 {
+    // The program won't run unless exactly one non-negative int is provided as argument
     if (argc != 2 || argv[1] < 0 || atoi(argv[1]) == 0)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    
+    // The 2nd argument (after the program name) is converted to int and will act as the key
     int k = atoi(argv[1]);
-    char plain[50];
-    char cipher[50];
+    char plain[50], cipher[50];
 
+    // Takes input from the user up until the carrier return key is entered
     printf("plaintext:  ");
     scanf("%49[^\n]", plain);
+
+    // Calls the cipher function which transforms plaintext -> ciphertext via address pointers
     caesar_cipher(k, plain, cipher);
     printf("ciphertext: %s\n", cipher);
 }
 
 void caesar_cipher(int k, char plain[], char cipher[])
 {
-    // printf("%d", k);
+    // Since the English alphabet only has 26 letters, rotating beyond this number is redundant
     k = k % 26;
-    // printf("%d", k);
-    char temp;
     int length = strlen(plain);
+    char temp;
+
     for (int i = 0; i < length; i++)
     {
         if (isalpha(plain[i]))
