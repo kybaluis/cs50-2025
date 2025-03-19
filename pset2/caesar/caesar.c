a5a9a004324d06e2a430526ca3e11883d963558d
Luis Quintino <luis.virtual@outlook.com>
2025-03-18 18:47:11 -0600

    cs50 2025 problem 2.3 main functionality done, pending one frownie face, comments and minor details

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 void caesar_cipher(int k, char plain[], char cipher[]);
 
@@ -16,18 +17,35 @@ int main(int argc, char *argv[])
     char cipher[50];
 
     printf("plaintext:  ");
-    scanf("%49s", plain);
+    scanf("%49[^\n]", plain);
     caesar_cipher(k, plain, cipher);
     printf("ciphertext: %s\n", cipher);
 }
 
 void caesar_cipher(int k, char plain[], char cipher[])
 {
+    // printf("%d", k);
+    k = k % 26;
+    // printf("%d", k);
     char temp;
     int length = strlen(plain);
     for (int i = 0; i < length; i++)
     {
-        temp = plain[i] + k;
+        if (isalpha(plain[i]))
+        {
+            if (plain[i] + k > 122)
+            {
+                temp = plain[i] + k - 26;
+            }
+            else
+            {
+                temp = plain[i] + k;
+            }
+        }
+        else
+        {
+            temp = plain[i];
+        }
         cipher[i] = temp;
     }
     cipher[length] = '\0';
