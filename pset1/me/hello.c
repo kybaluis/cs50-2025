c8f02059312bfd570c1951f1a123bbe07f137ff6
Luis Quintino <72029375+kybaluis@users.noreply.github.com>
2025-06-02 07:21:50 -0600

    Added description summary

--- pset1/me/hello.c
+++ pset1/me/hello.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+ * me.c
+ * CS50 2025 pset 1.1
+ * -------------------------------------------
+ * Simply prompts the user for their name and prints a personalized greeting.
+ */
+
 int main(void)
 {
     char name[50];
